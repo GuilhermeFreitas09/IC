2789049069f1b4ccdbf180bc9f3e835f0c531351
GuilhermeFreitas09 <g.pereirafreitas09@gmail.com>
2022-01-27 21:08:00 -0300

    Nova branch dedicada ao ambiente cenapad

--- R/check_dependencies.R
+++ R/check_dependencies.R
@@ -1,49 +1,51 @@
 # Checa dependências
 my_dependencies_check <- function(){
 
-  if (!require("BiocManager", quietly = TRUE)){
-    install.packages("BiocManager")
-    BiocManager::install(version = "3.13")}
+  ll <- '/home/lovelace/proj/proj858/gpereira/pacotesR'
 
-  if(!require("limma")){BiocManager::install("limma")}
+  if (!require("BiocManager", quietly = TRUE, lib.loc=ll){
+    install.packages("BiocManager", lib = ll)
+    BiocManager::install(version = "3.13", lib = ll)}
 
-  if(!require("minfi")){BiocManager::install("minfi")}
+  if(!require("limma", lib.loc = ll)){BiocManager::install("limma", lib = ll)}
 
-  if(!require("minfiData")){BiocManager::install("minfiData")}
+  if(!require("minfi", lib.loc = ll)){BiocManager::install("minfi", lib = ll)}
 
-  if(!require("missMethyl")){BiocManager::install("missMethyl")}
+  if(!require("minfiData", lib.loc = ll)){BiocManager::install("minfiData", lib = ll)}
 
-  if(!require("IlluminaHumanMethylationEPICanno.ilm10b4.hg19")){
-    BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19")}
+  if(!require("missMethyl", lib.loc = ll)){BiocManager::install("missMethyl", lib = ll)}
 
-  if(!require("IlluminaHumanMethylationEPICmanifest")){
-    BiocManager::install("IlluminaHumanMethylationEPICmanifest")}
+  if(!require("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", lib.loc = ll)){
+    BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", lib = ll)}
 
-  if(!require("maxprobes")){
-    if(!require("remotes")){install.packages("remotes")}
-    remotes::install_github("markgene/maxprobres")
+  if(!require("IlluminaHumanMethylationEPICmanifest", lib.loc = ll)){
+    BiocManager::install("IlluminaHumanMethylationEPICmanifest", lib = ll)}
+
+  if(!require("maxprobes", lib.loc = ll)){
+    if(!require("remotes", lib.loc = ll){install.packages("remotes", lib = ll)}
+    remotes::install_github("markgene/maxprobres", lib = ll)
   }
 
-  if(!require("dplyr")){install.packages("dplyr")}
+  if(!require("dplyr", lib.loc = ll)){install.packages("dplyr", lib = ll)}
 
-  if(!require("ggplot2")){install.packages("ggplot2")}
+  if(!require("ggplot2", lib.loc = ll)){install.packages("ggplot2", lib = ll)}
 
-  if(!require("crayon")){install.packages("crayon")}
+  if(!require("crayon", lib.loc = ll)){install.packages("crayon", lib = ll)}
 
 }
 
 # Carrega os pacotes
 my_libraries <- function(){
 
-  library(limma)
-  library(minfi)
-  library(minfiData)
-  library(missMethyl)
-  library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
-  library(IlluminaHumanMethylationEPICmanifest)
-  library(maxprobes)
-  library(dplyr)
-  library(ggplot2)
-  library(crayon)
+  library(limma, lib.loc = ll)
+  library(minfi, lib.loc = ll)
+  library(minfiData, lib.loc = ll)
+  library(missMethyl, lib.loc = ll)
+  library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19, lib.loc = ll)
+  library(IlluminaHumanMethylationEPICmanifest, lib.loc = ll)
+  library(maxprobes, lib.loc = ll)
+  library(dplyr, lib.loc = ll)
+  library(ggplot2, lib.loc = ll)
+  library(crayon, lib.loc = ll)
 
 }
